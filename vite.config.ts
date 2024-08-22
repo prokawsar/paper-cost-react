f664dff5f9f0ba75e5ac07334b481e574a5635b2
Kawsar Ahmed <prokawsar@gmail.com>
2024-08-22 20:13:25 +0600

    CRA to Vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@/*": path.resolve(__dirname, "src/*"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@types": path.resolve(__dirname, "src/types/"),
+        "@db": path.resolve(__dirname, "src/db/"),
+        "@store": path.resolve(__dirname, "src/store"),
+        "@utils": path.resolve(__dirname, "src/utils/"),
+      },
+    },
+  };
+});
